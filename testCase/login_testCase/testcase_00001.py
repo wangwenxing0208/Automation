be9bd29880f4ff65451ea643ca1cf9dfcd0f5f68
star <18519323020@163.com>
2022-04-05 21:51:46 +0800

    修改

--- testCase/login_testCase/testcase_00001.py
+++ testCase/login_testCase/testcase_00001.py
@@ -16,16 +16,22 @@ import unittest
 from page.login.login import Login
 from page.login.init import Init
 from page.login.userHome import HomePage
-from utils.assertion import Assert
+from utils.logger import Logger
 import time
 
+logger = Logger(logger='TestCase').getlog()
 
-class TestcaseLogin(Init, Login, HomePage, Assert):
+class TestcaseLogin(Init,Login,HomePage):
     def test_Login(self):
         '''验证登陆成功'''
         self.login('wwx', '123qwe')
         time.sleep(3)
-        self.assertequal('王文星', self.UserName())
+        try:
+            self.assertEqual('王文星1', self.UserName())
+            logger.info('预期结果：王文星1，测试结果：%s，测试通过' % self.UserName())
+        except:
+            logger.error('预期结果：王文星1，测试结果：%s，测试不通过' % self.UserName())
+            raise
 
 if __name__ == '__main__':
     unittest.main(verbosity=2)
